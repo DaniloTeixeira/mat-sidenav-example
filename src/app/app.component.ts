b332f7600e97ec5416d883d91b2ec2440d7d04bd
Danilo Teixeira <danilo.rt@outlook.com>
2023-08-24 23:42:24 -0300

    Subindo projeto

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, MatSidenavModule, MatIconModule, MatButtonModule],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  opened = false;
+
+  setSidenavStatus(isSidenavOpen: boolean): void {
+    this.opened = isSidenavOpen;
+  }
+
+  onToggleMenuClick(): void {
+    this.opened = !this.opened;
+  }
+}
