5d1f584ee452d2ec814ec79e61d8f62a1019eda0
Danilo Teixeira <danilo.rt@outlook.com>
2023-10-29 01:52:44 -0300

    Incluindo roteamento e criando componente mat-select

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { SidenavComponent } from './core/components/sidenav';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [SidenavComponent],
+  imports: [ SidenavComponent, RouterModule ],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: [ './app.component.scss' ],
 })
 export class AppComponent {}
