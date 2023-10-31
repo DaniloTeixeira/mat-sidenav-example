1a523475d394a60824961687b23af9545e103a11
Danilo Teixeira <danilo.rt@outlook.com>
2023-10-31 00:11:16 -0300

    Refatorando o código

--- src/app/core/components/mat-select/mat-select.component.ts
+++ src/app/core/components/mat-select/mat-select.component.ts
@@ -1,12 +1,16 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormControl, NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
 
+interface Category {
+  title: string,
+  color: string;
+}
 @Component({
   selector: 'app-mat-select',
   standalone: true,
@@ -14,41 +18,36 @@ import { MatSelectModule } from '@angular/material/select';
   templateUrl: './mat-select.component.html',
   styleUrls: ['./mat-select.component.scss']
 })
-export class MatSelectComponent {
-  protected myControl = new FormControl(null);
 
-  protected categories = [
+export class MatSelectComponent {
+  private fb = inject(NonNullableFormBuilder);
+  protected form = this.buildForm();
+  protected myControl = new FormControl(null);
+  protected categories: Category[] = [
     {
-      idReminder: 0,
       title: 'Não iniciado',
-      description: '',
-      eventDate: new Date(),
       color: '#808080'
     },
     {
-      idReminder: 1,
       title: 'Pendente',
-      description: '',
-      eventDate: new Date(),
       color: '#ffa500'
     },
     {
-      idReminder: 2,
       title: 'Em andamento',
-      description: '',
-      eventDate: new Date(),
       color: '#0000ff'
     },
     {
-      idReminder: 3,
       title: 'Concluído',
-      description: '',
-      eventDate: new Date(),
       color: '#008000'
     },
   ];
 
   constructor() {
-    this.myControl.valueChanges.subscribe(console.log);
+  }
+
+  private buildForm() {
+    return this.fb.group({
+      myControl: [null as Category | null]
+    });
   }
 }
